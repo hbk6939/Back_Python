b5a23dbad326492260e78d45fec5ee47f35ed159
hbk6939 <hbk6939@gmail.com>
2022-08-09 16:12:05 +0900

    삼성 sw 아카데미 문제

---
+++ SW아카데미/1.py
@@ -0,0 +1,12 @@
+import sys; inp = sys.stdin.readline; out = sys.stdout.write
+# n = int(inp())
+# ls = list(map(int, inp().split()))
+# ls = [int(inp()) for _ in range(n)]
+# out(f"{}\n")
+
+
+T = int(inp())
+N = int(inp())
+
+ls = [list(map(int, inp().split())) for _ in range(N)]
+
---
+++ SW아카데미/2.py
@@ -0,0 +1,7 @@
+import sys; inp = sys.stdin.readline; outp = sys.stdout.write
+# n = int(inp())
+# ls = list(map(int, inp().split()))
+# ls = [int(inp()) for _ in range(n)]
+# outp(f"{}\n")
+
+
